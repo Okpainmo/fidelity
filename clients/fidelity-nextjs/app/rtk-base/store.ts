53a50894421782c260fef4e77ed927d438d1299e
Okpainmo <okpainmoandrew@gmail.com>
2026-04-06 16:16:44 +0100

    chore: complete base redux toolkit setup with ready-made auth slice

---
+++ clients/fidelity-nextjs/app/rtk-base/store.ts
@@ -0,0 +1,19 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+
+import authReducer from './slices/auth';
+
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+// Custom hooks for typed dispatch and selector
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export default store;
